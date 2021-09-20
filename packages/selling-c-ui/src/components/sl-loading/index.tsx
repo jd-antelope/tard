5705d28961dd8dcb284c1757fcf86416c077127a
chenyonghao9 <chenyonghao9@jd.com>
2021-09-20 19:04:10 +0800

    fix: test

--- packages/selling-c-ui/src/components/sl-loading/index.tsx
+++ packages/selling-c-ui/src/components/sl-loading/index.tsx
@@ -5,7 +5,7 @@ import { FC } from '@tarojs/taro'
 import { SlLoadingProps } from '../../../types/loading'
 import '../../style/components/sl-loading.less'
 const SlLoading: FC<SlLoadingProps> = () => {
-  return (<View className="aaa">3232</View>)
+  return (<View className="aaa">324432</View>)
 }
 
 export default memo(SlLoading)
