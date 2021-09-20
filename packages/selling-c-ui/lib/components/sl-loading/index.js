32511877748340e3b243f5179bcf87201429d8e1
chenyonghao9 <chenyonghao9@jd.com>
2021-09-20 19:14:05 +0800

    fix: ui

--- packages/selling-c-ui/lib/components/sl-loading/index.js
+++ packages/selling-c-ui/lib/components/sl-loading/index.js
@@ -2,7 +2,7 @@ import React, { memo } from 'react';
 import { View } from '@tarojs/components';
 import '../../style/components/sl-loading.less';
 const SlLoading = () => {
-    return (React.createElement(View, { className: "aaa" }, "777"));
+    return (React.createElement(View, { className: "aaa" }, "888"));
 };
 export default memo(SlLoading);
 //# sourceMappingURL=index.js.map
