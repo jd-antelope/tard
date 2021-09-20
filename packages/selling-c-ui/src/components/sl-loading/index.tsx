b1a2a450e3e0527442b265b89dc3c64f6927d91f
chenyonghao9 <chenyonghao9@jd.com>
2021-09-20 19:07:35 +0800

    fix: ui

--- packages/selling-c-ui/src/components/sl-loading/index.tsx
+++ packages/selling-c-ui/src/components/sl-loading/index.tsx
@@ -5,7 +5,7 @@ import { FC } from '@tarojs/taro'
 import { SlLoadingProps } from '../../../types/loading'
 import '../../style/components/sl-loading.less'
 const SlLoading: FC<SlLoadingProps> = () => {
-  return (<View className="aaa">324432</View>)
+  return (<View className="aaa">9999</View>)
 }
 
 export default memo(SlLoading)
