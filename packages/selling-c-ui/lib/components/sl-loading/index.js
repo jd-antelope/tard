73734298eab2756074184edce6b5e7a4160448fd
chenyonghao9 <chenyonghao9@jd.com>
2021-09-20 19:27:54 +0800

    fix: ui

--- packages/selling-c-ui/lib/components/sl-loading/index.js
+++ packages/selling-c-ui/lib/components/sl-loading/index.js
@@ -2,7 +2,7 @@ import React, { memo } from 'react';
 import { View } from '@tarojs/components';
 import '../../style/components/sl-loading.less';
 const SlLoading = () => {
-    return (React.createElement(View, { className: "aaa" }, "888"));
+    return (React.createElement(View, { className: "aaa" }, "1321321323"));
 };
 export default memo(SlLoading);
 //# sourceMappingURL=index.js.map
--- packages/selling-c-ui/src/components/sl-loading/index.tsx
+++ packages/selling-c-ui/src/components/sl-loading/index.tsx
@@ -5,7 +5,7 @@ import { FC } from '@tarojs/taro'
 import { SlLoadingProps } from '../../../types/loading'
 import '../../style/components/sl-loading.less'
 const SlLoading: FC<SlLoadingProps> = () => {
-  return (<View className="aaa">888</View>)
+  return (<View className="aaa">1321321323</View>)
 }
 
 export default memo(SlLoading)
