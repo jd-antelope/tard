1ec0c81918b5a5840da4cfbad11dc6197ea93536
chenyonghao9 <chenyonghao9@jd.com>
2021-09-26 15:04:35 +0800

    fix: test

--- packages/selling-c-ui/src/components/sl-upload/index.tsx
+++ packages/selling-c-ui/src/components/sl-upload/index.tsx
@@ -4,7 +4,8 @@ import { View } from '@tarojs/components'
 import { FC } from '@tarojs/taro'
 import '../../style/components/sl-upload.less'
 const SlUpload:FC = () => {
-  return <View className="sl-upload-wrapper">this ccc</View>
+  return <View className="sl-upload-wrapper">this ddd
+  </View>
 }
 
 export default memo(SlUpload)
