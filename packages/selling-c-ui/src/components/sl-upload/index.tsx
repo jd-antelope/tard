9b6a1927280d80bb7eb55548f23edc2b7cb553f8
chenyonghao9 <chenyonghao9@jd.com>
2021-09-27 12:03:07 +0800

    fix: ui

--- packages/selling-c-ui/src/components/sl-upload/index.tsx
+++ packages/selling-c-ui/src/components/sl-upload/index.tsx
@@ -4,7 +4,8 @@ import { View } from '@tarojs/components'
 import { FC } from '@tarojs/taro'
 import '../../style/components/sl-upload.less'
 const SlUpload:FC = () => {
-  return <View className="sl-upload-wrapper">ggg
+  return <View className="sl-upload-wrapper">fff
+
   </View>
 }
 
