cc5676322110b9b26e7a54d0504addfe6780028d
huozhongyi <huozhongyi@jd.com>
2021-10-14 18:38:07 +0800

    feat: change toast component

--- packages/selling-c-ui/src/components/sl-toast/index.tsx
+++ packages/selling-c-ui/src/components/sl-toast/index.tsx
@@ -102,18 +102,18 @@ export default class SlToast extends React.Component<
     /* eslint-enable @typescript-eslint/no-non-null-assertion */
 
     const bodyClass = classNames('toast-body', {
-      'sl-c-toast__body--custom-image': image,
+      'slc-toast__body--custom-image': image,
       'toast-body--text': !realImg && !icon,
       [`sl-toast__body--${status}`]: !!status
     })
 
-    const iconClass = classNames('sl-c-icon', {
-      [`sl-c-icon-${icon}`]: icon
+    const iconClass = classNames('slc-icon', {
+      [`slc-icon-${icon}`]: icon
     })
 
     return _isOpened ? (
-      <View className={classNames('sl-c-toast', this.props.className)}>
-        {hasMask && <View className='sl-c-toast__overlay' />}
+      <View className={classNames('slc-toast', this.props.className)}>
+        {hasMask && <View className='slc-toast__overlay' />}
         <View
           className={bodyClass}
           style={customStyle}
