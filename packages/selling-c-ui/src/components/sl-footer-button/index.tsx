807e6b7e6c3238eb9b9be3b23f156d1c78ae27e8
jinmingwei <jinmingwei@jd.com>
2021-10-22 18:38:46 +0800

    feat: 底部按钮完善

--- packages/selling-c-ui/src/components/sl-footer-button/index.tsx
+++ packages/selling-c-ui/src/components/sl-footer-button/index.tsx
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import { View } from '@tarojs/components'
 import { SlFooterButtonProps, SlFooterButtonState } from '../../../types/sl-footer-button'
 import PropTypes, {InferProps} from "prop-types";
+import { objectToString } from '../../common/utils'
 
 export default class SlFooterButton extends React.Component<SlFooterButtonProps, SlFooterButtonState> {
   public static defaultProps: SlFooterButtonProps
@@ -13,17 +14,48 @@ export default class SlFooterButton extends React.Component<SlFooterButtonProps,
   }
 
   private handleClick() {
-    this.props.clickBtn && this.props.clickBtn(arguments as any)
+    this.props.onlick && this.props.onlick(arguments as any)
+  }
+
+  private handleClickSecBtn() {
+    this.props.secOnlick && this.props.secOnlick(arguments as any)
   }
 
   // eslint-disable-next-line no-undef
   public render (): JSX.Element | null {
+    const containerStyle = (objectToString({
+      padding: this.props.padding,
+      // padding-bottom: constant(safe-area-inset-bottom),
+      // padding-bottom: env(safe-area-inset-bottom);
+    }))
+
+    const style = (objectToString({
+      color: this.props.color,
+      background: this.props.background,
+      'border-radius': this.props.radius,
+      width: this.props.doubleBtn? '50%' : '100%',
+      lineHeight: this.props.lineHeight
+    }))
+
+    const style2 = (objectToString({
+      color: this.props.secColor,
+      background: this.props.secBackground,
+      'border-radius': this.props.radius,
+      width: '50%',
+      lineHeight: this.props.lineHeight
+    }))
+
     return (
-      <View className="slc-custom-footer-container">
+      <View className="slc-custom-footer-container" style={containerStyle}>
         { this.props.replaceContent ?
             <Fragment>{ this.props.replaceContent }</Fragment>
-        :
-            <View className="slc-custom-footer" onClick={this.handleClick.bind(this)}>{this.props.btnName}</View>
+            :
+            <View className='slc-custom-outer'>
+              {
+                this.props.doubleBtn && <View className="slc-custom-footer slc-custom-footer-btn2" style={style2} onClick={this.handleClickSecBtn.bind(this)}>{this.props.secondName}</View>
+              }
+              <View className="slc-custom-footer slc-custom-footer-btn1" style={style} onClick={this.handleClick.bind(this)}>{this.props.name}</View>
+            </View>
         }
       </View>
     )
@@ -31,11 +63,26 @@ export default class SlFooterButton extends React.Component<SlFooterButtonProps,
 }
 
 SlFooterButton.defaultProps = {
-  btnName: '按钮'
+  name: '按钮',
+  padding: '20px',
+  radius: '0',
+  lineHeight: '40px',
+  doubleBtn: false,
+  secondName: '取消',
 }
 
 SlFooterButton.propTypes = {
-  btnName: PropTypes.string,
-  clickBtn: PropTypes.func,
-  replaceContent: PropTypes.any
+  name: PropTypes.string,
+  onlick: PropTypes.func,
+  replaceContent: PropTypes.any,
+  color: PropTypes.string,
+  background: PropTypes.string,
+  padding: PropTypes.string,
+  radius: PropTypes.string,
+  lineHeight: PropTypes.string,
+  doubleBtn: PropTypes.bool,
+  secondName: PropTypes.string,
+  secColor: PropTypes.string,
+  secBackground: PropTypes.string,
+  secOnlick: PropTypes.func,
 }
--- packages/selling-c-ui/types/sl-footer-button.d.ts
+++ packages/selling-c-ui/types/sl-footer-button.d.ts
@@ -1,21 +1,66 @@
 
 import SlComponent from './base'
+import {ComponentClass} from "react";
 
 export interface SlFooterButtonProps extends SlComponent {
     /**
      * 按钮名字
      * @default 按钮
      */
-    btnName?: string
+    name?: string
     /**
      * 点击事件
      */
-    clickBtn?: CommonEventFunction
+    onlick?: CommonEventFunction
     /**
      * 自定义按钮内容
      */
-    replaceContent?: any
+    replaceContent?: React.ReactNode
+    /**
+     * 按钮颜色
+     */
+    color?: string
+    /**
+     * 填充颜色
+     */
+    background?: string
+    /**
+     * 外边框的padding
+     */
+    padding?: string
+    /**
+     * 按钮radius
+     */
+    radius?: string
+    /**
+     * line-height
+     */
+    lineHeight?: string
+    /**
+     * 是否为双按钮
+     */
+    doubleBtn?: boolean
+    /**
+     * 第二个按钮文字
+     */
+    secondName?: string
+    /**
+     * 第二个按钮按钮颜色
+     */
+    secColor?: string
+    /**
+     * 第二个按钮填充颜色
+     */
+    secBackground?: string
+    /**
+     * 点击事件
+     */
+    secOnlick?: CommonEventFunction
 }
 
 export interface SlFooterButtonState {
 }
+
+declare const SlFooterButton: ComponentClass<SlFooterButtonProps>
+
+export default SlFooterButton
