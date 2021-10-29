cce9588ec7950b402d25d7f1d57f3c7e7f9ba81f
chenlei736 <chenlei736@jd.com>
2021-10-29 20:42:53 +0800

    feat: add doc

--- packages/selling-c-docs/src/pages/docs/rate.tsx
+++ packages/selling-c-docs/src/pages/docs/rate.tsx
@@ -1,15 +1,30 @@
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
-const markdown = `## api
+const markdown = `# rate
+评分组件，可自定义评分星星图标的大小、间隔、评分数
+
+## 使用指南
+在 Taro 文件中引入组件
+~~~js
+import { SlRate } from '@jd/selling-c-ui'
+~~~
+
+
+## api
 |  属性   | 说明  | 类型 | 默认值 |
 |  ----  | ----  | ---- | ---- |
-| size | 评分星星大小 | number; | 20 |
-| value | 当前评分,开发者需要通过onChange事件来更新value值，必填 | number; | - |
-| max | 最大评分 | number; | 5 |
-| margin | 星星间隔,单位根据环境自动转为rpx或rem | number; | 5 |
-| onChange | 输入框值改变时触发的事件，开发者需要通过onChange事件来更新value值变化，但不填写onChange函数时，该组件只读 | CommonEventFunction; | - |
-| activeColor | 高亮的星星的颜色 | string | - |
+| value | 当前评分,需要通过 onChange 事件来更新 value 值，必填 | Number | 0 |
+| max | 最大 | Number | 5 |
+| size | 评分星星大小 | string | - |
+| margin | 按钮颜色 | Number / String	 | 20 |
+| activeColor | 选中星星的颜色 | string | '#ffca3e' |
+
+
+## 事件
+|  事件名称   | 说明  | 返回参数 |
+|  ----      | ---- |   ----  |
+|  onChange  | 输入框值改变时触发的事件，开发者需要通过 onChange 事件来更新 value 值变化，但不填写 onChange 函数时，该组件只读  | 当前值 value |
 `
 
 export default function DocsPage() {
