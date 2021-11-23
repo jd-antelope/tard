0b002e34ad0334d4bad1aa7ccc80c2a9a0d7b7be
huozhongyi <huozhongyi@jd.com>
2021-11-23 14:59:28 +0800

    feat: change canvas component

--- packages/selling-c-ui/src/components/canvas/index.tsx
+++ packages/selling-c-ui/src/components/canvas/index.tsx
@@ -2,6 +2,6 @@ import { isWeb } from '../../common/utils'
 import SlCanvasWeapp from './weapp'
 import SlCanvasH5 from './h5'
 
-const SlCanvas = isWeb() ? SlCanvasH5 : SlCanvasWeapp
+const SlCanvas = isWeb ? SlCanvasH5 : SlCanvasWeapp
 
 export default SlCanvas
