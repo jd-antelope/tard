ad70ae746d77a34abf7ee7bff80b604148147a49
huozhongyi <huozhongyi@jd.com>
2021-12-04 14:14:42 +0800

    feat: change docs index page

---
+++ packages/selling-c-docs/src/business/constants.ts
@@ -0,0 +1,1 @@
+export const IMG_PREFIX = 'https://storage.360buyimg.com/hawley-common/tard-image'
--- packages/selling-c-docs/src/pages/index.tsx
+++ packages/selling-c-docs/src/pages/index.tsx
@@ -1,47 +1,35 @@
 import { history } from 'umi';
 import { Button } from 'antd';
-import styles from './index.less';
+import { IMG_PREFIX } from '@/business/constants'
+import './index.less';
 
 export default function IndexPage() {
   return (
-    <div className={ styles.indexpage }>
-      <div className="night">
-        <div className="shooting_star"></div>
-        <div className="shooting_star"></div>
-        <div className="shooting_star"></div>
-        <div className="shooting_star"></div>
-        <div className="shooting_star"></div>
-        <div className="shooting_star"></div>
-        <div className="shooting_star"></div>
-        <div className="shooting_star"></div>
-        <div className="shooting_star"></div>
-        <div className="shooting_star"></div>
-        <div className="shooting_star"></div>
-        <div className="shooting_star"></div>
-        <div className="shooting_star"></div>
-        <div className="shooting_star"></div>
-        <div className="shooting_star"></div>
-        <div className="shooting_star"></div>
-        <div className="shooting_star"></div>
-        <div className="shooting_star"></div>
-        <div className="shooting_star"></div>
-        <div className="shooting_star"></div>
-      </div>
-      <div className={ styles.header }>
-        <h2>selling UI</h2>
-        <h3>一套基于商羚C端的 Taro 框架开发的多端 UI 组件库</h3>
-        <div className={ styles.buttons }>
-          <Button 
-            className={styles.button} 
-            onClick={ () => history.push('/docs') } 
-            type="primary"
-          >起步</Button>
-          <Button 
-            className={styles.button} 
-            onClick={ () => window.location.href = 'https://coding.jd.com/selling-front/shop-c-components/' } 
-            danger
-          >coding</Button>
+    <div className="index-page">
+      <div className="container index-page__banner">
+        <div className="index-page__banner-content">
+          <div className="index-page__banner-content__login">
+            <img src={ `${IMG_PREFIX}/login-big.png` } />
+          </div>
+          <p className="index-page__banner-content__p">一套基于Taro框架开发的多端React UI组件库</p>
+          <div className="index-page__banner-content__buttons">
+            <Button 
+              className="index-page__banner-content__button" 
+              onClick={ () => history.push('/docs') } 
+              type="primary"
+            >起步</Button>
+            <Button
+              className="index-page__banner-content__button"
+              onClick={ () => window.location.href = 'https://coding.jd.com/selling-front/shop-c-components/' } 
+              type="primary"
+              ghost
+            >扫码体验</Button>
+          </div>
         </div>
+      </div>
+      <div className="container index-page__features">组件特色</div>
+      <div className="container index-page__footer">
+        <div className=""></div>
       </div>
     </div>
   );
