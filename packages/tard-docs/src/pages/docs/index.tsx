05fa09119f71cf7d0091d5f72b5e9ee62131f2c3
zhengxiuyue <zhengxiuyue@jd.com>
2021-12-11 15:13:15 +0800

    docs

--- packages/tard-docs/src/pages/docs/index.tsx
+++ packages/tard-docs/src/pages/docs/index.tsx
@@ -29,8 +29,8 @@ Tard是**京东商羚前端团队**开源移动端多端组件库, 开发和服�
 + Android 5.0+
 + iOS 9.2+
 ## 贡献者
-![Alt](https://avatars.githubusercontent.com/u/51318010?v=4)
-![Alt](https://avatars.githubusercontent.com/u/29476081?v=4)
+![Alt](https://avatars.githubusercontent.com/u/51318010?v=4 100*100)
+![Alt](https://avatars.githubusercontent.com/u/29476081?v=4 100*100)
 
 ## 链接
 + 仓库地址
