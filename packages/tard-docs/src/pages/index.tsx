cecb0d69805bce84130e052ca4a14ccecc2aead4
huozhongyi <huozhongyi@jd.com>
2022-01-04 14:54:03 +0800

    feat: add docs index page

--- packages/tard-docs/src/pages/index.tsx
+++ packages/tard-docs/src/pages/index.tsx
@@ -1,18 +1,19 @@
 import { history } from 'umi';
 import { Button } from 'antd';
+import cn from 'classnames';
 import { IMG_PREFIX } from '@/business/constants'
 import { logoUrl } from '@/business/layout'
-import FooterResources from '@/business/home-footer-resources'
+import { COMPS_LIST, FOOTTER_RESOURCES } from '@/business/home-resources'
 import './index.less';
 
 export default function IndexPage() {
   return (
-    <div className="index-page">
+    <div className="index-page index-bg">
       <div className="index-page__banner">
         <div className="container">
           <div className="index-page__banner-content">
             <div className="index-page__banner-content__login">
-              <img src={ `${IMG_PREFIX}/login-big.png` } />
+              <img src={ `${IMG_PREFIX}/logo-big.png` } />
             </div>
             <p className="index-page__banner-content__p">一套基于Taro框架开发的多端React UI组件库</p>
             <div className="index-page__banner-content__buttons">
@@ -36,7 +37,29 @@ export default function IndexPage() {
           </div>
         </div>
       </div>
-      <div className="container index-page__features">组件特色</div>
+      <div className="container index-page__features comps">
+        <div className="comps-header">组件特色</div>
+        <div className="comps-content">
+          {
+            COMPS_LIST.map((v, i) => (
+              <div className="comps-content-item" key={ i }>
+                <div className="comps-content-item__title">{ v.title }</div>
+                <div className="comps-content-item__content">{ v.content }</div>
+                <div className={
+                    cn('comps-content-item__des', { 
+                      'comps-content-item__desfrond' : v.content === '', 
+                      'family-light' : v.content !== '' 
+                    })
+                  }>{ v.des }</div>
+                {
+                  v.isTip && <div className="comps-content-item__tip">持续扩展中</div>
+                }
+                <img className="comps-content-item__bg" src={ v.url } />
+              </div>
+            ))
+          }
+        </div>
+      </div>
       <div className="index-page__footer">
         <div className="container container-footer">
           <div className="index-page__footer-list">
@@ -46,15 +69,16 @@ export default function IndexPage() {
             />
           </div>
           {
-            FooterResources.map((v, i) => (
+            FOOTTER_RESOURCES.map((v, i) => (
               <div className="index-page__footer-list" key={ i }>
                 <div className="index-page__footer-header">{ v.title }</div>
                 {
                   v.children.map((val, idx) =>(
                     <div 
                       className="index-page__footer-item" key={ idx }
-                      onClick={ val.clickCallback }
-                    >{val.title}</div>
+                    >
+                      <a href={ val.url }>{val.title}</a>
+                    </div>
                   ))
                 }
               </div>
