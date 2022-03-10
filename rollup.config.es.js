a639a7d10772d4f541547a6144b4606d6c26cc85
huozhongyi <huozhongyi@jd.com>
2022-03-10 10:58:35 +0800

    feat: change styles

--- rollup.config.es.js
+++ rollup.config.es.js
@@ -6,6 +6,7 @@ import RollupTypescript from 'rollup-plugin-typescript2'
 import { uglify as RollupUglify } from 'rollup-plugin-uglify'
 import RollupCopy from 'rollup-plugin-copy'
 import RollupPluginDemo from './scripts/watch-demo/index'
+import RollupPluginStyle from './scripts/rollup-plugin-style'
 
 const resolveFile = p => path.resolve(__dirname, '.', p)
 
@@ -64,25 +65,10 @@ export default {
         {
           src: resolveFile('packages/ui/src/styles'),
           dest: resolveFile('packages/ui/dist')
-        },
-        {
-          src: resolveFile('packages/ui/src/components/*/*.less'),
-          dest: resolveFile('packages/ui/dist/styles/components')
-        },
-        {
-          src: resolveFile('packages/ui/src/index.d.ts'),
-          dest: resolveFile('packages/ui/dist/types')
-        },
-        {
-          src: resolveFile('packages/ui/src/components/*/*.d.ts'),
-          dest: resolveFile('packages/ui/dist/types/components'),
-          rename: (_, __, fullPath) => {
-            const reg = /components\/([\s\S]*)\//
-            return `${fullPath.match(reg)[1]}.d.ts`
-          }
-        },
+        }
       ]
     }),
+    RollupPluginStyle({ watch: process.env.NODE_ENV  === 'development'}),
     RollupPluginDemo({ watch: process.env.NODE_ENV  === 'development'}),
   ]
 }
---
+++ scripts/rollup-plugin-style.js
@@ -0,0 +1,74 @@
+const globby = require('globby')
+const path = require("path");
+const fs = require('fs-extra');
+const nodeWatch = require('node-watch');
+
+const cwd = process.cwd()
+const staticLink = 'packages/ui/dist/styles/components'
+const reg = /components\/([\s\S]*)\//
+
+// 获取demo所有文件
+const getDemoPath = () => {
+	const packagePaths = globby.sync(`${cwd}/packages/ui/src/components/*/*.less`)
+	return getPathIndex(packagePaths)
+}
+
+const getPathIndex = (packagePaths) => {
+  const arr = []
+  packagePaths.map(file => {
+    let isDown = -1
+    const fileName = file.match(reg)[1]
+    for (let i = 0; i < arr.length; i++) {
+      if (fileName === arr[i].name) {
+        isDown = i
+        break
+      }
+    }
+    if (isDown === -1) {
+      arr.push({
+        name: fileName,
+        packagePaths: [file]
+      })
+    } else {
+      arr[isDown].packagePaths.push(file)
+    }
+  })
+  return arr
+}
+
+const writeDemoFile = (res) => {
+  const check = fs.existsSync(`${cwd}/${staticLink}/${res.name}`)
+  if (!check) fs.mkdirSync(`${cwd}/${staticLink}/${res.name}`)
+  res.packagePaths.map(val => {
+    const newFile = val.split('components/')[1]
+    console.log(newFile)
+    fs.copyFileSync(
+      val, 
+      `${cwd}/${staticLink}/${newFile}`
+    )
+  })
+}
+
+const start = () => {
+  const list = getDemoPath();
+  const check = fs.existsSync(`${cwd}/${staticLink}`)
+  if (!check) fs.mkdirSync(`${cwd}/${staticLink}`)
+  list.map(v => writeDemoFile(v))
+}
+
+start()
+
+export default function RollupPluginDemo ({ watch }) {
+	return {
+		name: "rollup-plugin-style", // rollup插件名称，必须符合格式
+		buildStart (source) {
+			if (watch) {
+				nodeWatch(path.join(__dirname, 'packages/ui/src/components'), { recursive: true }, function (evt, name) {
+					name.includes('/demo/') && start();
+				});
+			}
+			start();
+		},
+		load () {}
+	}
+}
