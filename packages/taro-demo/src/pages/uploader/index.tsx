b17a54b90571bad948f6e01c6bbb39590629dee4
huozhongyi <huozhongyi@jd.com>
2022-03-24 15:47:35 +0800

    feat: add uploader comps

---
+++ packages/taro-demo/src/pages/uploader/index.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { FC } from '@tarojs/taro'
+import { View } from '@tarojs/components'
+import { Uploader } from 'haw-ui-test'
+import DocsHeader from '../../components/doc-header'
+
+const UploaderPage: FC = () => {
+  const [videos, setVideos] = useState<any>([]);
+  const [iamges, setImages] = useState<any>([])
+  const [iamges1, setImages1] = useState<any>([])
+  return (
+    <View className="container full-container">
+      <DocsHeader title='Uploader 文件上传' />
+      <View className='doc-body'>
+        <View className='doc-body-content-tip'>基本用法</View>
+        <View className='doc-body-content__info doc-body-content__row'>
+          <Uploader
+            mediaType="video"
+            multiple
+            length={3} maxCount={1}
+            files={videos} onChange={(v) => { setVideos(v) }}
+          />
+        </View>
+        <View className='doc-body-content__info doc-body-content__row'>
+          <Uploader
+            mediaType="camera" length={3} count={3}
+            files={iamges} onChange={(v) => { setImages(v) }}
+          />
+        </View>
+        <View className='doc-body-content__info doc-body-content__row'>
+          <Uploader
+            mediaType="camera" length={3} count={3}
+            files={iamges1} onChange={(v) => { setImages1(v) }}
+          >
+            <View>测试文本</View>
+          </Uploader>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default UploaderPage
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -25,11 +25,12 @@ import SearchBar from './components/search-bar'
 import Canvas from './components/canvas'
 import FooterButton from './components/footer-button'
 import DatetimePicker from './components/datetime-picker'
+import Uploader from './components/uploader'
 
 export { 
   Button, DropdownMenu, ConfigProvider, Icon, Image, 
   Price, Toast, Loading, Form, Field, Rate, Switch,
   Popup, Sort, Empty, Skeleton, TagPrice, Grid, Badge,
   InputNumber, Modal, NavBar, Overlay, SearchBar, Canvas,
-  FooterButton, DatetimePicker
+  FooterButton, DatetimePicker, Uploader
 }
